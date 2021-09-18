688f1ff7fd08099d910faeb82cd93a10fc7c0406
Wade Lieurance <wlieurance@gmail.com>
2021-09-18 15:39:53 -0700

    added incomplete unit test module

---
+++ python/tests/test_classes.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+# class TestSqlParser(unittest.TestCase):
+#
+#     def test_consume_food_consumes_the_apple(self):
+#         s = SqlParser(text='')
+#         self.assertTrue(s.apple.consumed,
+#                         "Expected apple to be consumed")
+#
+#     def test_consume_food_cuts_the_food(self):
+#         c = Consumer()
+#         c.consume_food()
+#         self.assertTrue(c.apple.been_cut,
+#                         "Expected apple to be cut")
+#
+#     def test_pick_food_always_selects_the_apple(self):
+#         c = Consumer()
+#         food = c.pick_food()
+#         self.assertEquals(c.apple, food,
+#                           "Expected apple to have been picked")
+
+
+if __name__ == '__main__':
+    unittest.main()
